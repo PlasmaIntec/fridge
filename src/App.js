4e4624276e9f94a282fb6b80f2744b9dd2608dab
PlasmaIntec <alphaintec@gmail.com>
2019-04-11 18:56:26 -0700

    Add functionality to add folders

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import { connect } from 'react-redux'
+import { addFolder } from './actions/addFolder'
+import { renameFolder } from './actions/renameFolder'
+
+import Folder from './containers/Folders'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const mapDispatchToProps = dispatch => ({
+  handleAddFolder: (folderName) => dispatch(addFolder(folderName)),
+  handleRenameFolder: (oldFolderId, newFolderName) => dispatch(renameFolder(oldFolderId, newFolderName))
+})
+
+const mapStateToProps = state => ({
+  ...state
+})
+
+const App = ({ folders, handleAddFolder }) => {
+  let input;
+  return (
+    <div className="App">
+      {
+        folders && folders.map(folder => (
+          <div key={folder.id}>
+            {folder.name}
+          </div>
+        ))
+      }
+      <form onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        handleAddFolder(input.value)
+        input.value = ''
+      }}>
+        <input ref={node => {
+          input = node
+        }} />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  )
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
